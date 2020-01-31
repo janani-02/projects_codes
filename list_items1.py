6011f8f39a9f51a4543792c8d72f5ca566e5dec7
janani-02 <60179684+janani-02@users.noreply.github.com>
2020-02-01 00:11:23 +0530

    list creations

---
+++ list_items1.py
@@ -0,0 +1,22 @@
+list = []
+print("\n intial blank list:")
+print(list)
+
+
+list = ['groceries']
+print("\n list using string:")
+print(list)
+
+list=["groceries","fruits","flowers"]
+print("\n list contains multiple values:")
+print(list[0])
+print(list[1])
+print(list[2])
+
+list = [1,2,3,3,4,4,5,7]
+print("\n list having numbers:")
+print(list)
+
+list = [1,2,'fruits',3,'flowers',4,7]
+print("\n list with uses of mixed values:")
+print(list)
